0bad0150fd04dc7ce1aa4b3249be61d19766c3c5
Sushant Sutar <sushant.sutR@gmail.com>
2026-01-15 21:12:04 +0530

    Refactor Two Sum solution for clarity

--- EASY/TWO-SUM/0001_Two-Sum.cpp
+++ EASY/TWO-SUM/0001_Two-Sum.cpp
@@ -6,23 +6,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
+    vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> mp;
-
-        for (int i = 0; i < nums.size(); i++)
-        {
-            int need = target - nums[i];
-
-            if (mp.find(need) != mp.end())
-            {
-                return {mp[need], i};
+        for (int i = 0; i < nums.size(); i++) {
+            int x = nums[i];
+            int  need;
+            need = target - x;
+            if (mp.find(need) != mp.end()) {
+                return {mp[need],i};
+            } else {
+                mp[x] = i;
             }
-            mp[nums[i]] = i;
         }
         return {};
     }
-};
+    };
+
+
