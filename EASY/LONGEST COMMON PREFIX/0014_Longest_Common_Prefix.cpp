7cbbee47fdb6459766de9f04f9c290a4f9d6e153
Sushant Sutar <sushant.sutR@gmail.com>
2026-01-23 22:50:52 +0530

    Update 0014_Longest_Common_Prefix.cpp

--- EASY/LONGEST COMMON PREFIX/0014_Longest_Common_Prefix.cpp
+++ EASY/LONGEST COMMON PREFIX/0014_Longest_Common_Prefix.cpp
@@ -1,23 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-     if(strs.empty()){
-        return "";
-     }  
-
-     string Prefix = strs[0];
-
-     for(int i=1;i<strs.size();i++){
-        while(strs[i].find(Prefix) != 0){
-            Prefix.pop_back();
-        }
-
-        if(Prefix.empty()){
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        if (strs.empty())
+        {
             return "";
         }
-     }
-     return Prefix;
+
+        string Prefix = strs[0];
+
+        for (int i = 1; i < strs.size(); i++)
+        {
+            while (strs[i].find(Prefix) != 0)
+            {
+                Prefix.pop_back();
+            }
+
+            if (Prefix.empty())
+            {
+                return "";
+            }
+        }
+        return Prefix;
     }
 };
